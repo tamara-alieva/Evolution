a35f038babd8daac43f0b78fbcd6dde31089ca41
Tamara <tamaraavgust14@gmail.com>
2024-06-25 00:38:06 +0700

    Добавление структуры-сети нейронов

--- libs/neural-network/src/lib.rs
+++ libs/neural-network/src/lib.rs
@@ -1,14 +1,8 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+#[derive(Debug)]
+pub struct Network;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl Network {
+    pub fn propagate(&self, inputs: Vec<f32>) -> Vec<f32> {
+        todo!()
     }
 }
