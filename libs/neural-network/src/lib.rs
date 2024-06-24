d41b2024f3a7d6ba6e4198510a4fcc657e6effe7
Tamara <tamaraavgust14@gmail.com>
2024-06-25 01:10:02 +0700

    Добавление структур Слой и Нейрон

--- libs/neural-network/src/lib.rs
+++ libs/neural-network/src/lib.rs
@@ -1,8 +1,27 @@
 #[derive(Debug)]
 pub struct Network;
 
-impl Network {
-    pub fn propagate(&self, inputs: Vec<f32>) -> Vec<f32> {
+impl Network {  // Нейронная сеть
+    pub fn propagate(&self, mut inputs: Vec<f32>) -> Vec<f32> {
+        self.layers
+            .iter()
+            .fold(inputs, |inputs, layer| layer.propagate(inputs))
+    }
+}
+
+#[derive(Debug)]
+struct Layer {  // Слой сети
+    neurons: Vec<Neuron>,
+}
+
+#[derive(Debug)]
+struct Neuron {         // Нейрон
+    bias: f32,          // Смещение
+    weights: Vec<f32>   // Весы
+}
+
+impl Layer {
+    fn propagate(&self, inputs: Vec<f32>) -> Vec<f32> {
         todo!()
     }
 }
