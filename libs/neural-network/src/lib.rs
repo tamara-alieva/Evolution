2c53caffe94d74b05cdc5c238eb5add5655e9569
Tamara <tamaraavgust14@gmail.com>
2024-06-25 01:23:40 +0700

    Редактирование методов для Слоя и Нейрона

--- libs/neural-network/src/lib.rs
+++ libs/neural-network/src/lib.rs
@@ -1,3 +1,5 @@
+use std::process::Output;
+
 #[derive(Debug)]
 pub struct Network;
 
@@ -14,14 +16,35 @@ struct Layer {  // Слой сети
     neurons: Vec<Neuron>,
 }
 
+impl Layer {
+    fn propagate(&self, inputs: Vec<f32>) -> Vec<f32> {
+        self.neurons
+            .iter()
+            .map(|neuron| neuron.propagate(&inputs))
+            .collect()
+    }
+}
+
 #[derive(Debug)]
 struct Neuron {         // Нейрон
     bias: f32,          // Смещение
     weights: Vec<f32>   // Весы
 }
 
-impl Layer {
-    fn propagate(&self, inputs: Vec<f32>) -> Vec<f32> {
-        todo!()
+impl Neuron {
+    fn propagate(&self, inputs: &[f32]) -> f32 {
+        let mut output = 0.0;
+
+        for i in 0..inputs.len() {
+            output += inputs[i] * self.weights[i];
+        }
+
+        output += self.bias;
+
+        if output > 0.0 {
+            output
+        } else {
+            0.0
+        }
     }
 }
