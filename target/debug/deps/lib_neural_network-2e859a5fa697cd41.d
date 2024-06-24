268f179aede748076799772185f460635130fa5a
Tamara <tamaraavgust14@gmail.com>
2024-06-25 00:37:44 +0700

    Initial commit

---
+++ target/debug/deps/lib_neural_network-2e859a5fa697cd41.d
@@ -0,0 +1,5 @@
+D:\practice\Evolution\target\debug\deps\liblib_neural_network-2e859a5fa697cd41.rmeta: libs\neural-network\src\lib.rs
+
+D:\practice\Evolution\target\debug\deps\lib_neural_network-2e859a5fa697cd41.d: libs\neural-network\src\lib.rs
+
+libs\neural-network\src\lib.rs:
---
+++ target/debug/deps/lib_neural_network-fb1cf9165173eb3d.d
@@ -0,0 +1,5 @@
+D:\practice\Evolution\target\debug\deps\liblib_neural_network-fb1cf9165173eb3d.rmeta: libs\neural-network\src\lib.rs
+
+D:\practice\Evolution\target\debug\deps\lib_neural_network-fb1cf9165173eb3d.d: libs\neural-network\src\lib.rs
+
+libs\neural-network\src\lib.rs:
