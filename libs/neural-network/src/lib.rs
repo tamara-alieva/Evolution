bf33a980d8fa6704ec1df409b4c5ed6376ab6152
Tamara <tamaraavgust14@gmail.com>
2024-06-26 21:57:33 +0700

    Добавление тестов для Нейронной сети; изменение тестов для Нейронов; конструкторы

--- libs/neural-network/src/lib.rs
+++ libs/neural-network/src/lib.rs
@@ -6,12 +6,16 @@ pub struct Network {
 }
 
 #[derive(Debug)]
-pub struct LayerTopology {
+pub struct LayerTopology { // Топология слоёв
     pub neurons: usize,
 }
 
 impl Network {  // Нейронная сеть
-    pub fn random(rng: &mut dyn RngCore, layers: &[LayerTopology]) -> Self {
+    pub fn new(layers: Vec<Layer>) -> Self { // конструктор
+        Self { layers }
+    }
+
+    pub fn random(rng: &mut dyn RngCore, layers: &[LayerTopology]) -> Self { // рандомайзер
         assert!(layers.len() > 1);
 
         let layers = layers
@@ -22,7 +26,7 @@ impl Network {  // Нейронная сеть
         Self { layers }
     }
 
-    pub fn propagate(&self, mut inputs: Vec<f32>) -> Vec<f32> {
+    pub fn propagate(&self, mut inputs: Vec<f32>) -> Vec<f32> { // распространение
         self.layers
             .iter()
             .fold(inputs, |inputs, layer| layer.propagate(inputs))
@@ -35,6 +39,15 @@ struct Layer {  // Слой сети
 }
 
 impl Layer {
+    pub fn new(neurons: Vec<Neuron>) -> Self {
+        assert!(!neurons.is_empty());
+        assert!(neurons
+            .iter()
+            .all(|neuron| neuron.weights.len() == neurons[0].weights.len()));
+
+        Self { neurons }
+    }
+
     fn random(rng: &mut dyn RngCore, input_size: usize, output_size: usize) -> Self {
         let neurons = (0..output_size)
             .map(|_| Neuron::random(rng, input_size))
@@ -58,6 +71,11 @@ struct Neuron {         // Нейрон
 }
 
 impl Neuron {
+    pub fn new(bias: f32, weights: Vec<f32>) -> Self { // конструктор
+        assert!(!weights.is_empty());
+        Self { bias, weights }
+    }
+
     fn random(rng: &mut dyn RngCore, input_size: usize) -> Self {
         let mut rng = rand::thread_rng();
         let bias = rng.gen_range(-1.0..=1.0);
@@ -82,6 +100,7 @@ impl Neuron {
     }
 }
 
+// Тесты
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -90,35 +109,104 @@ mod tests {
     use rand_chacha::ChaCha8Rng;
 
     #[test]
-    fn random() {
+    fn random() { // рандомайзер
         let mut rng = ChaCha8Rng::from_seed(Default::default());
+
+        //---------------------------------------------------------------------
+
+        // Нейрон
         let neuron = Neuron::random(&mut rng, 4);
-
         assert_relative_eq!(neuron.bias, -0.6255188);
-
         assert_relative_eq!(
             neuron.weights.as_slice(),
-            [0.67383957, 0.8181262, 0.26284897, 0.5238807].as_ref()
+            &[0.67383957, 0.8181262, 0.26284897, 0.5238807].as_slice()
         );
+
+        //---------------------------------------------------------------------
+
+        // Нейронная сеть
+        let network = Network::random(
+            &mut rng,
+            &[
+                LayerTopology { neurons: 3 },
+                LayerTopology { neurons: 2 },
+                LayerTopology { neurons: 1 },
+            ],
+        );
+        assert_eq!(network.layers.len(), 2);
+        assert_eq!(network.layers[0].neurons.len(), 2);
+        assert_relative_eq!(network.layers[0].neurons[0].bias, -0.6255188);
+        assert_relative_eq!(
+            network.layers[0].neurons[0].weights.as_slice(),
+            &[0.67383957, 0.8181262, 0.26284897].as_slice()
+        );
+        assert_relative_eq!(network.layers[0].neurons[1].bias, 0.5238807);
+        assert_relative_eq!(
+            network.layers[0].neurons[1].weights.as_slice(),
+            &[-0.5351684, 0.069369555, -0.7648182].as_slice()
+        );
+        assert_eq!(network.layers[1].neurons.len(), 1);
+        assert_relative_eq!(
+            network.layers[1].neurons[0].weights.as_slice(),
+            &[-0.48879623, -0.19277143].as_slice()
+        );
+
+        //---------------------------------------------------------------------
+
     }
 
     #[test]
     fn propagate() {
+        // Нейрон
+
         let neuron = Neuron {
             bias: 0.5,
             weights: vec![-0.3, 0.8],
         };
-    
-        // Ensures `.max()` (our ReLU) works:
         assert_relative_eq!(
             neuron.propagate(&[-10.0, -10.0]),
             0.0,
         );
-    
-        // `0.5` and `1.0` chosen by a fair dice roll:
         assert_relative_eq!(
             neuron.propagate(&[0.5, 1.0]),
             (-0.3 * 0.5) + (0.8 * 1.0) + 0.5,
         );
+
+        /*#[test]
+        fn returns_propagated_input() { // распространяемое значение
+            let actual = Neuron::new(0.1, vec![-0.3, 0.6, 0.9]).propagate(&[0.5, -0.6, 0.7]);
+            let expected: f32 = 0.1 + (0.5 * -0.3) + (-0.6 * 0.6) + (0.7 * 0.9);
+            approx::assert_relative_eq!(actual, expected.max(0.0));
+        }
+
+        #[test]
+        fn restricts_output() { // ограничение выходного значения
+            let neuron = Neuron::new(0.0, vec![0.5]);
+            let v1 = neuron.propagate(&[-1.0]);
+            let v2 = neuron.propagate(&[-0.5]);
+            let v3 = neuron.propagate(&[0.0]);
+            let v4 = neuron.propagate(&[0.5]);
+            let v5 = neuron.propagate(&[1.0]);
+            assert_relative_eq!(v1, v2);
+            assert_relative_eq!(v2, v3);
+            assert_relative_ne!(v3, v4);
+            assert_relative_ne!(v4, v5);
+        }*/
+
+        //---------------------------------------------------------------------
+
+        // Нейронная сеть
+        let layers = (
+            Layer::new(vec![
+                Neuron::new(0.0, vec![-0.5, -0.4, -0.3]),
+                Neuron::new(0.0, vec![-0.2, -0.1, 0.0]),
+            ]),
+            Layer::new(vec![Neuron::new(0.0, vec![-0.5, 0.5])]),
+        );
+        //let network = Network::new(vec![layers.0.clone(), layers.1.clone()]);
+        let actual = network.propagate(vec![0.5, 0.6, 0.7]);
+        let expected = layers.1.propagate(layers.0.propagate(vec![0.5, 0.6, 0.7]));
+        assert_relative_eq!(actual.as_slice(), expected.as_slice());
+
     }
 }
