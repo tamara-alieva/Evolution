10120af1685293efbbf2349e70f8abd006cf2c62
Tamara <tamaraavgust14@gmail.com>
2024-06-29 08:44:17 +0700

    Добавление трейтов Индивид и Метод селекции

--- libs/genetic-algorithm/src/lib.rs
+++ libs/genetic-algorithm/src/lib.rs
@@ -1,7 +1,17 @@
+use rand::seq::SliceRandom;
+use rand::{Rng, RngCore};
+
 pub struct GeneticAlgorithm; // Генетический алгоритм
 
+pub trait Individual {
+    fn fitness(&self) -> f32;
+}
+
 impl GeneticAlgorithm {
-    pub fn evolve<I>(&self, population: &[I]) -> Vec<I> {
+    pub fn evolve<I>(&self, population: &[I]) -> Vec<I> 
+    where
+        I: Individual 
+    {
         assert!(!population.is_empty());
 
         (0..population.len())
@@ -12,5 +22,25 @@ impl GeneticAlgorithm {
                 todo!()
             })
             .collect()
+    }
+}
+
+
+pub trait SelectionMethod {
+    fn select<'a, I>(&self, rng: &mut dyn RngCore, population: &'a [I]) -> &'a I
+    where 
+    I: Individual;
+}
+
+pub struct RouletteWheelSelection;
+
+impl SelectionMethod for RouletteWheelSelection {
+    fn select<'a, I>(&self, rng: &mut dyn RngCore, population: &'a [I]) -> &'a I
+    where 
+        I: Individual,
+    {
+        population
+            .choose_weighted(rng, |individual| individual.fitness())
+            .expect("got an empty population")
     }
 }
