f03c5d22a63752b5df97608f7e7631fd33acd5ff
Tamara <tamaraavgust14@gmail.com>
2024-06-29 10:35:15 +0700

    Добавление тестов

--- libs/genetic-algorithm/src/lib.rs
+++ libs/genetic-algorithm/src/lib.rs
@@ -3,7 +3,7 @@ use rand::{Rng, RngCore};
 
 pub struct GeneticAlgorithm; // Генетический алгоритм
 
-pub trait Individual {
+pub trait Individual { // Индивид
     fn fitness(&self) -> f32;
 }
 
@@ -26,13 +26,13 @@ impl GeneticAlgorithm {
 }
 
 
-pub trait SelectionMethod {
+pub trait SelectionMethod { // Метод селекции
     fn select<'a, I>(&self, rng: &mut dyn RngCore, population: &'a [I]) -> &'a I
     where 
     I: Individual;
 }
 
-pub struct RouletteWheelSelection;
+pub struct RouletteWheelSelection; // Колесо-рулетка
 
 impl SelectionMethod for RouletteWheelSelection {
     fn select<'a, I>(&self, rng: &mut dyn RngCore, population: &'a [I]) -> &'a I
@@ -43,4 +43,65 @@ impl SelectionMethod for RouletteWheelSelection {
             .choose_weighted(rng, |individual| individual.fitness())
             .expect("got an empty population")
     }
+}
+
+// Тесты
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rand::SeedableRng;
+    use rand_chacha::ChaCha8Rng;
+    use std::collections::BTreeMap;
+    use std::iter::FromIterator;
+
+    #[test]
+    fn roulette_wheel_selection() {
+        let mut rng = ChaCha8Rng::from_seed(Default::default());
+        let population = vec![
+            TestIndividual::new(2.0),
+            TestIndividual::new(1.0),
+            TestIndividual::new(4.0),
+            TestIndividual::new(3.0),
+        ];
+
+        let mut actual_histogram = BTreeMap::new();
+
+        for _ in 0..1000 {
+            let fitness = RouletteWheelSelection
+                .select(&mut rng, &population)
+                .fitness() as i32;
+
+            *actual_histogram
+                .entry(fitness)
+                .or_insert(0) += 1;
+        }
+
+        let expected_histogram = BTreeMap::from_iter([
+            (1, 98), // (здоровье, сколько раз оно уже было выбрано)
+            (2, 202),
+            (3, 278),
+            (4, 422),
+        ]);
+
+        assert_eq!(actual_histogram, expected_histogram);
+
+    }
+
+    #[derive(Clone, Debug)]
+    struct TestIndividual {
+        fitness: f32,
+    }
+
+    impl TestIndividual {
+        fn new(fitness: f32) -> Self {
+            Self { fitness }
+        }
+    }
+
+    impl Individual for TestIndividual {
+        fn fitness(&self) -> f32 {
+            self.fitness
+        }
+    }
+
 }
