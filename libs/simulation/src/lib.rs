4aebe3664998f2a880c39f7e54a9a05e304c8b48
Tamara <tamaraavgust14@gmail.com>
2024-06-30 16:31:06 +0700

    Добавление геттеров

--- libs/simulation/src/lib.rs
+++ libs/simulation/src/lib.rs
@@ -30,41 +30,65 @@ pub struct Point2 { // Точка
 }
 
 impl Simulation {
-    pub fn random(rng: &mut dyn RngCore) -> Self {
+    pub fn random(rng: &mut dyn RngCore) -> Self { // конструктор-рандомайзер
         Self {
             world: World::random(rng),
         }
     }
+
+    pub fn world(&self) -> &World { // геттер (мир)
+        &self.world
+    }
 }
 
 impl World {
-    pub fn random(rng: &mut dyn RngCore) -> Self {
+    pub fn random(rng: &mut dyn RngCore) -> Self { // конструктор-рандомайзер
         let animals = (0..40)
             .map(|_| Animal::random(rng))
             .collect();
 
         let foods = (0..60)
-            .map(|_| food::random(rng))
+            .map(|_| Food::random(rng))
             .collect();
 
         Self { animals, foods }
     }
+
+    pub fn animals(&self) -> &[Animal] {
+        &self.animals
+    }
+
+    pub fn foods(&self) -> &[Food] {
+        &self.foods
+    }
 }
 
 impl Animal {
-    pub fn random(rng: &mut dyn RngCore) -> Self {
+    pub fn random(rng: &mut dyn RngCore) -> Self { // конструктор-рандомайзер
         Self {
             position: rng.gen(),
             rotation: rng.gen(),
             speed: 0.002,
         }
     }
+
+    pub fn position(&self) -> na::Point2<f32> {
+        self.position
+    }
+
+    pub fn rotation(&self) -> na::Rotation2<f32> {
+        self.rotation
+    }
 }
 
 impl Food {
-    pub fn random(rng: &mut dyn RngCore) -> Self {
+    pub fn random(rng: &mut dyn RngCore) -> Self { // конструктор-рандомайзер
         Self {
             position: rng.gen(),
         }
+    }
+
+    pub fn position(&self) -> na::Point2<f32> {
+        self.position
     }
 }
