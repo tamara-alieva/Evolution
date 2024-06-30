b066e1653635a2f4c27c86e9cef92f1b6c1c16aa
Tamara <tamaraavgust14@gmail.com>
2024-06-30 22:14:12 +0700

    Исправление ошибки с исчезанием птиц спустя некоторое время после начала симуляции

--- libs/simulation/src/lib.rs
+++ libs/simulation/src/lib.rs
@@ -65,6 +65,9 @@ impl Simulation {
     pub fn step(&mut self) {
         for animal in &mut self.world.animals {
             animal.position += animal.rotation * na::Vector2::new(0.0, animal.speed);
+
+            animal.position.x = na::wrap(animal.position.x, 0.0, 1.0);
+            animal.position.y = na::wrap(animal.position.y, 0.0, 1.0);
         }
     }
 }
