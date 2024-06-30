ba4a818c72bec5d854cac837e4256c6edd170eab
Tamara <tamaraavgust14@gmail.com>
2024-06-30 21:42:20 +0700

    Вывод каждой птицы на экран в виде треугольника

--- libs/simulation-wasm/src/lib.rs
+++ libs/simulation-wasm/src/lib.rs
@@ -35,6 +35,7 @@ impl From<&sim::World> for World {
 pub struct Animal {
     pub x: f32,
     pub y: f32,
+    pub rotation: f32,
 }
 
 #[wasm_bindgen]
@@ -56,6 +57,7 @@ impl From<&sim::Animal> for Animal {
         Self {
             x: animal.position().x,
             y: animal.position().y,
+            rotation: animal.rotation().angle(),
         }
     }
 }
