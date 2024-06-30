faa06ee661dbdcb89688ab467a350c96c8607265
Tamara <tamaraavgust14@gmail.com>
2024-06-30 16:47:41 +0700

    Обёртка для WebAssembly

--- libs/simulation-wasm/src/lib.rs
+++ libs/simulation-wasm/src/lib.rs
@@ -1,6 +1,20 @@
+use lib_simulation as sim;
+use rand::prelude::*;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn whos_that_dog() -> String {
-    "Mister Peanutbutter".into()
+pub struct Simulation {
+    rng: ThreadRng,
+    sim: sim::Simulation,
+}
+
+#[wasm_bindgen]
+impl Simulation {
+    #[wasm_bindgen(constructor)]
+    pub fn new() -> Self {
+        let mut rng = thread_rng();
+        let sim = sim::Simulation::random(&mut rng);
+
+        Self { rng, sim }
+    }
 }
