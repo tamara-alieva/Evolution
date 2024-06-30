d7e6c4b18c7093e2967b4723969c5ad123d3b748
Tamara <tamaraavgust14@gmail.com>
2024-06-30 16:39:23 +0700

    Добавление зависимости (rand) + директория simulation

---
+++ target/debug/deps/lib_simulation_wasm-3d1fcef203d6b3cc.d
@@ -0,0 +1,5 @@
+D:\practice\Evolution\target\debug\deps\liblib_simulation_wasm-3d1fcef203d6b3cc.rmeta: libs\simulation-wasm\src\lib.rs
+
+D:\practice\Evolution\target\debug\deps\lib_simulation_wasm-3d1fcef203d6b3cc.d: libs\simulation-wasm\src\lib.rs
+
+libs\simulation-wasm\src\lib.rs:
