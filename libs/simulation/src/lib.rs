6520014762b6d6a8ec249d6cabfb01d6422e31f4
Tamara <tamaraavgust14@gmail.com>
2024-06-30 10:42:37 +0700

    Добавление структур Симуляция, Мир, Животное, Еда, Точка

--- libs/simulation/src/lib.rs
+++ libs/simulation/src/lib.rs
@@ -1,6 +1,25 @@
-pub struct Simulation {
+pub struct Simulation { // Симуляция
     world: World,
 }
 
 #[derive(Debug)]
-pub struct World;
+pub struct World { // Мир
+    animals: Vec<Animal>,
+    foods: Vec<Food>,
+}
+
+#[derive(Debug)]
+pub struct Animal { // Животное
+    position: Point2,
+}
+
+#[derive(Debug)]
+pub struct Food { // Еда
+    position: Point2,
+}
+
+#[derive(Debug)]
+pub struct Point2 { // Точка
+    x: f32,
+    y: f32,
+}
