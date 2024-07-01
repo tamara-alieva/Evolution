37100eacf33b56f12391d218c37d53fbae9fb7bd
Tamara <tamaraavgust14@gmail.com>
2024-07-01 20:29:22 +0700

    Commit

--- target/debug/deps/lib_simulation_wasm-5879e3a1959908d4.d
+++ target/debug/deps/lib_simulation_wasm-5879e3a1959908d4.d
@@ -1,5 +1,5 @@
-D:\practice\Evolution\target\debug\deps\liblib_simulation_wasm-5879e3a1959908d4.rmeta: libs\simulation-wasm\src\lib.rs
+D:\Evolution\Evolution\target\debug\deps\liblib_simulation_wasm-5879e3a1959908d4.rmeta: libs\simulation-wasm\src\lib.rs
 
-D:\practice\Evolution\target\debug\deps\lib_simulation_wasm-5879e3a1959908d4.d: libs\simulation-wasm\src\lib.rs
+D:\Evolution\Evolution\target\debug\deps\lib_simulation_wasm-5879e3a1959908d4.d: libs\simulation-wasm\src\lib.rs
 
 libs\simulation-wasm\src\lib.rs:
--- target/debug/deps/lib_simulation_wasm-b68b8df979ba9c82.d
+++ target/debug/deps/lib_simulation_wasm-b68b8df979ba9c82.d
@@ -1,5 +1,5 @@
-D:\practice\Evolution\target\debug\deps\liblib_simulation_wasm-b68b8df979ba9c82.rmeta: libs\simulation-wasm\src\lib.rs
+D:\Evolution\Evolution\target\debug\deps\liblib_simulation_wasm-b68b8df979ba9c82.rmeta: libs\simulation-wasm\src\lib.rs
 
-D:\practice\Evolution\target\debug\deps\lib_simulation_wasm-b68b8df979ba9c82.d: libs\simulation-wasm\src\lib.rs
+D:\Evolution\Evolution\target\debug\deps\lib_simulation_wasm-b68b8df979ba9c82.d: libs\simulation-wasm\src\lib.rs
 
 libs\simulation-wasm\src\lib.rs:
